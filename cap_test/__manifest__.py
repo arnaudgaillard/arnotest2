547400d37d118b32667c5dfc3d8c7eefb9e1e309
arnaudgaillard <43142326+arnaudgaillard@users.noreply.github.com>
2019-01-11 13:32:06 +0000

    Ajout de cap_test au versionning

---
+++ cap_test/__manifest__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo v12 EE.
+
+{
+'name': 'cap_test',
+'author': 'captivea-aga',
+'version': '0.1',
+'description': """
+This module is used to manage... test AGA - Call me GOD !
+""",
+'depends': [],
+'data': [],
+'installable': True,	
+}
+
