f6c6cbfcedc2d1761098b7a5682ee53e1cd63e24
arnaudgaillard <43142326+arnaudgaillard@users.noreply.github.com>
2019-01-11 15:38:28 +0100

    Update manifest.

--- cap_aga/__manifest__.py
+++ cap_aga/__manifest__.py
@@ -2,14 +2,13 @@
 # Part of CAPTIVEA. Odoo v12 EE.
 
 {
-'name': 'cap_aga',
-'author': 'captivea-aga',
-'version': '0.2',
-'description': """
-This module is used to manage... test AGA - Call me GOD !
-""",
-'depends': [],
-'data': [],
-'installable': True,	
+  'name': 'cap_aga',
+  'author': 'captivea-aga',
+  'version': '0.2',
+  'description': """
+  This module is used to manage... test AGA - Call me GOD !
+  """,
+  'depends': [],
+  'data': [],
+  'installable': True
 }
-
